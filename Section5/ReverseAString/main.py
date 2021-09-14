3aee9215e155462246b4e1ae0097d345be2c615a
Mandha Smitha <smandhasmithas61@gmail.com>
2021-09-14 15:42:28 +0400

    Reverse a string - optimized.

--- Section5/ReverseAString/main.py
+++ Section5/ReverseAString/main.py
@@ -1,10 +1,9 @@
 # Reverse a string
 
 def reverse_string(input_text: str):
-    input_array = list(input_text)
     reversed_string = ""
-    for index in range(len(input_array) - 1, -1, -1):
-        reversed_string = reversed_string + input_array[index]
+    for index in range(len(input_text) - 1, -1, -1):
+        reversed_string = reversed_string + input_text[index]
     return reversed_string
 
 
